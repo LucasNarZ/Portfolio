72eb37421f8bdd0cf753f9c288e5e4e398c8c013
LucasNarZ <lucasnarloch123@gmail.com>
2025-06-11 16:51:58 -0300

    create career timeline

---
+++ src/components/careerTimeline.tsx
@@ -0,0 +1,59 @@
+import { ArrowRight, ArrowLeft } from "lucide-react";
+
+type CareerStep = {
+    date: string;
+    title: string;
+    description: string;
+};
+
+const careerData: CareerStep[] = [
+    {
+        date: "Oct 2024 - now",
+        title: "internship BackEnd Developer",
+        description: "My first professional experience.",
+    },
+    {
+        date: "Jan 2025 - now",
+        title: "Bachelor in Computer Science at PUCPR",
+        description: "Started a bachelor in Computer Science to go deeper into programming knowledge.",
+    },
+];
+
+export default function CareerTimeline() {
+    return (
+        <div className="relative flex flex-col items-center px-4 py-10">
+            <div className="absolute left-1/2 top-0 h-full w-1 bg-gray-300 transform -translate-x-1/2" />
+
+            {careerData.map((step, index) => {
+                const isLeft = index % 2 === 0;
+                return (
+                    <div
+                        key={index}
+                        className={`relative w-full flex items-center mb-5 transition-all duration-500 ${!isLeft ? "justify-start" : "justify-end"
+                            }`}
+                    >
+                        <div className="w-1/2 px-4 group">
+                            <div className="bg-gradient-to-br from-white to-gray-50 shadow-lg group-hover:shadow-2xl rounded-2xl p-4 transform transition-all duration-300 group-hover:scale-[1.03]">
+                                <p className="text-sm text-gray-400">{step.date}</p>
+                                <h3 className="text-xl font-bold text-gray-800">{step.title}</h3>
+                                <p className="text-gray-600 mt-2">{step.description}</p>
+                            </div>
+                        </div>
+
+                        <div className="absolute left-1/2 transform -translate-x-1/2 z-10 bg-white p-2 rounded-full shadow-md group-hover:shadow-xl transition-all duration-300 animate-pulse">
+                            <div className="transition-transform duration-300 group-hover:scale-125">
+                                {isLeft ? (
+                                    <ArrowRight className="text-gray-500" />
+                                ) : (
+                                    <ArrowLeft className="text-gray-500" />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+
+
+    );
+}
