8b830b5bec3ca8bb70f943f3785509456b1d3b9f
LucasNarZ <lucasnarloch123@gmail.com>
2025-06-11 16:53:12 -0300

    change layout and add career timeline

--- src/sections/aboutMe.tsx
+++ src/sections/aboutMe.tsx
@@ -1,8 +1,15 @@
+import CareerTimeline from "../components/careerTimeline"
+
 function AboutMe(){
     return (
-        <section className="flex w-[100vw] h-[100vw] bg-gray-100 dark:bg-gray-900 justify-center">
-            <h1 className="text-5xl mt-20">About Me</h1>
-
+        <section className="flex flex-col w-[100vw] h-[100vh] bg-gray-100 dark:bg-gray-900 items-center">
+            <h1 className="text-5xl mt-20 mb-12">About Me</h1>
+            <div className="flex">
+                <div className="flex items-center justify-center">
+                    <p>asdadsadsad</p>
+                </div>
+                <CareerTimeline />
+            </div>
             
         </section>
     )
