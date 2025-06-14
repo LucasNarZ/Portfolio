82d6e37efd3f48a1d37e034be2171986c2dc1cb3
LucasNarZ <LucasNarloch123@gmail.com>
2025-06-13 21:23:49 -0300

    begin skills section

---
+++ src/sections/skills.tsx
@@ -0,0 +1,11 @@
+function Skills(){
+    return (
+        <section id="Skills" className="flex flex-col w-[100vw] h-[130vh] bg-gray-100 dark:bg-gray-900 items-center">
+            <h1 className="text-4xl mt-10 mb-4 text-black dark:text-white">Skills</h1>
+            <p className="mb-12 text-black dark:text-white">The skills that permit me to create anything.</p>
+            
+        </section>
+    )
+}
+
+export default Skills
