4d8abe4f507aab89822802dcbc174efcab1ed9c3
LucasNarZ <LucasNarloch123@gmail.com>
2025-06-13 21:24:21 -0300

    finish about me section

--- src/sections/aboutMe.tsx
+++ src/sections/aboutMe.tsx
@@ -2,12 +2,9 @@ import CareerTimeline from "../components/careerTimeline"
 
 function AboutMe(){
     return (
-        <section className="flex flex-col w-[100vw] h-[100vh] bg-gray-100 dark:bg-gray-900 items-center">
-            <h1 className="text-5xl mt-20 mb-12">About Me</h1>
+        <section id="AboutMe" className="flex flex-col w-[100vw] h-[130vh] bg-gray-100 dark:bg-gray-900 items-center">
+            <h1 className="text-4xl mt-10 mb-12 text-black dark:text-white">About Me</h1>
             <div className="flex">
-                <div className="flex items-center justify-center">
-                    <p>asdadsadsad</p>
-                </div>
                 <CareerTimeline />
             </div>
             
